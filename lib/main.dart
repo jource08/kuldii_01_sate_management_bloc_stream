379c52f170e97a240a6ee40039991b63401228b8
Mahmud Jamal <mahmudjamal08@gmail.com>
2022-09-17 15:55:18 +0700

    init

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+  Stream<int> countStream() async* {
+    for (int i = 1; i <= 10; i++) {
+      await Future.delayed(const Duration(seconds: 1));
+      yield i;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    debugPrint('REBUILD');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Stream App"),
+      ),
+      body: StreamBuilder(
+        stream: countStream(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            return Center(
+              child: Text("${snapshot.data}",
+                  style: const TextStyle(fontSize: 50)),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
